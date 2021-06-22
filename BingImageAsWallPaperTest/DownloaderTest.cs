d5840b34dede3f5d80533eaf8bdaa35831b19dce
Noah Song <noah.song@aspiraconnect.com>
2021-06-22 16:10:34 +0800

    add debug info

--- BingImageAsWallPaperTest/DownloaderTest.cs
+++ BingImageAsWallPaperTest/DownloaderTest.cs
@@ -74,6 +74,7 @@ namespace BingImageAsWallPaperTest
 
         private void cleanImageFile()
         {
+            _testOutputHelper.WriteLine("***cleanImageFile***");
             var fileUtil = fixture.serviceProvider.GetRequiredService<FileUtil>();
             var testFiles = new List<string> { "bing-20210616-OHR.BrightEye_ZH-CN6196887876_UHD.jpg", 
                                                "bing-20210527-OHR.ICanHearIt_EN-US7945824197_UHD.jpg", 
