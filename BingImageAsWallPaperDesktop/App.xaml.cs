59e4129f3d41e9988de08f6f8e2c884001fa08b4
Noah Song <noah.song@aspiraconnect.com>
2021-06-28 16:35:09 +0800

    my first wpf project

---
+++ BingImageAsWallPaperDesktop/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using BingImageAsWallPaper;
+using BingImageAsWallPaper.ImageDownload;
+using BingImageAsWallPaper.Option;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BingImageAsWallPaperDesktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddLogging(configure => { configure.AddDebug(); });
+
+            services.AddSingleton(x =>
+               new FileOption { ImagePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "bingwallpaper") }
+            );
+            services.AddHttpClient();
+            services.AddTransient<IDownloader, DownloaderService>();
+            services.AddTransient<FileUtil>();
+            services.AddTransient<Wallpaper>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ BingImageAsWallPaperDesktop/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using BingImageAsWallPaper;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BingImageAsWallPaperDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ILogger<MainWindow> _logger;
+        private readonly Wallpaper _wallpaper;
+        public MainWindow(ILogger<MainWindow> logger, Wallpaper wallpaper)
+        {
+            InitializeComponent();
+            _logger = logger;
+            _wallpaper = wallpaper;
+        }
+
+        private void Random_Set_WallPaper(object sender, RoutedEventArgs e)
+        {
+            _wallpaper.SetRandom();
+        }
+    }
+}
