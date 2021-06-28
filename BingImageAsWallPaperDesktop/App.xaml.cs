951d6832abfa464b62b027eb6020d28f0cfc57cf
Noah Song <noah.song@aspiraconnect.com>
2021-06-28 17:30:43 +0800

    min to system tray

--- BingImageAsWallPaperDesktop/App.xaml.cs
+++ BingImageAsWallPaperDesktop/App.xaml.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,13 +21,26 @@ namespace BingImageAsWallPaperDesktop
     public partial class App : Application
     {
         private readonly ServiceProvider _serviceProvider;
+        System.Windows.Forms.NotifyIcon nIcon = new System.Windows.Forms.NotifyIcon();
 
         public App()
         {
             var serviceCollection = new ServiceCollection();
             ConfigureServices(serviceCollection);
             _serviceProvider = serviceCollection.BuildServiceProvider();
+            nIcon.Icon = new Icon(Path.Combine(AppContext.BaseDirectory, "wallpaper.ico"));
+            nIcon.Visible = true;
+            nIcon.ShowBalloonTip(3000, "WallPaper", "Set WallPaper", System.Windows.Forms.ToolTipIcon.Info);
+            nIcon.DoubleClick += nIcon_Click;
         }
+
+        private void nIcon_Click(object sender, EventArgs e)
+        {
+            //events comes here
+            MainWindow.Visibility = Visibility.Visible;
+            MainWindow.WindowState = WindowState.Normal;
+        }
+
         private void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<MainWindow>();
--- BingImageAsWallPaperDesktop/MainWindow.xaml.cs
+++ BingImageAsWallPaperDesktop/MainWindow.xaml.cs
@@ -48,5 +48,11 @@ namespace BingImageAsWallPaperDesktop
         {
             await _downloader.DownloadAll();
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visibility = Visibility.Hidden;
+        }
     }
 }
