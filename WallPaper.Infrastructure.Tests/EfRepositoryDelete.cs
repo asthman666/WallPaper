28949a351ff8327a1baee87311388cc497f79ff5
Noah Song <noah.song@aspiraconnect.com>
2021-10-14 11:00:35 +0800

    add update and delete test

---
+++ WallPaper.Infrastructure.Tests/EfRepositoryDelete.cs
@@ -0,0 +1,28 @@
+using BingImageAsWallPaper.Entity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WallPaper.Infrastructure.Tests
+{
+    public class EfRepositoryDelete : BaseEfRepoTestFixture
+    {
+        [Fact]
+        public async Task DeletesItemAfterAddingIt()
+        {
+            // add an item
+            var repository = GetRepository();
+            var initialTitle = Guid.NewGuid().ToString();
+            var item = new WallPaperDbEntityBuilder().Title(initialTitle).Build();
+            await repository.AddAsync(item);
+
+            // delete the item
+            await repository.DeleteAsync(item);
+
+            // verify it's no longer there
+            Assert.DoesNotContain(await repository.ListAsync<WallPaperDbEntity>(),
+                i => i.ImageName == initialTitle);
+        }
+    }
+}
---
+++ WallPaper.Infrastructure.Tests/EfRepositoryUpdate.cs
@@ -0,0 +1,42 @@
+using BingImageAsWallPaper.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WallPaper.Infrastructure.Tests
+{
+    public class EfRepositoryUpdate : BaseEfRepoTestFixture
+    {
+        [Fact]
+        public async Task UpdatesItemAfterAddingIt()
+        {
+            // add an item
+            var repository = GetRepository();
+            var initialTitle = Guid.NewGuid().ToString();
+            var item = new WallPaperDbEntityBuilder().Title(initialTitle).Build();
+            await repository.AddAsync(item);
+
+            // detach the item so we get a different instance when using ListAsync to fetch this item from the database
+            _dbContext.Entry(item).State = EntityState.Detached;
+
+            // fetch the item and update its title
+            var newItem = (await repository.ListAsync<WallPaperDbEntity>())
+                    .FirstOrDefault(i => i.ImageName == initialTitle);
+            Assert.NotNull(newItem);
+            Assert.NotSame(item, newItem);
+
+            var newTitle = Guid.NewGuid().ToString();
+            newItem.ImageName = newTitle;
+
+            // Update the item
+            await repository.UpdateAsync(newItem);
+            var updatedItem = (await repository.ListAsync<WallPaperDbEntity>())
+                .FirstOrDefault(i => i.ImageName == newTitle);
+
+            Assert.NotNull(updatedItem);
+            Assert.NotEqual(item.ImageName, updatedItem.ImageName);
+        }
+    }
+}
