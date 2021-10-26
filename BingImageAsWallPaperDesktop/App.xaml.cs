cd8aceec37d705aeda0669bca13d26a30fdd0a83
Noah Song <noah.song@aspiraconnect.com>
2021-10-26 16:59:38 +0800

    desktop don't depend bingimage project

--- BingImageAsWallPaperDesktop/App.xaml.cs
+++ BingImageAsWallPaperDesktop/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using BingImageAsWallPaper;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
